b7828db7f89784aa74d94de4f03da5526c76d06e
Jakov764 <55740028+Jakov764@users.noreply.github.com>
2020-11-17 11:04:00 +0100

    Add files via upload

---
+++ vjezba4/vjezba4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite cijenu bez PDV-a: ");
+            double bPDV = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Unesite cijenu sa PDV-om: ");
+            double sPDV = Convert.ToDouble(Console.ReadLine());
+            double PDV = ((sPDV / bPDV) - 1) * 100;
+            Console.WriteLine("PDV izađe: " + PDV + "%");
+            Console.ReadKey();
+
+        }
+    }
+}
